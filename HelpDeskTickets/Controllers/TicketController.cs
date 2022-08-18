95c5e32d434ef44f8d350feb60bd8acf07554464
Kris <pranger.kris@gmail.com>
2022-08-18 11:59:05 -0400

    Working Endpoints

--- HelpDeskTickets/Controllers/TicketController.cs
+++ HelpDeskTickets/Controllers/TicketController.cs
@@ -16,23 +16,55 @@ namespace HelpDeskTickets.Controllers
             return context.Tickets.ToList();
         }
 
-        [HttpGet("GetTicketById")]
+        [HttpGet("GetTicketById/{id}")]
         public Ticket getTicketById(int id)
         {
             return context.Tickets.FirstOrDefault(t => t.TicketId == id);
         }
 
-        [HttpGet("GetByStatus")]
+        [HttpGet("GetByStatus/{status}")]
         public List<Ticket> getTicketByStatus(string status)
         {
             return context.Tickets.Where(t => t.Status == status).ToList();
         }
 
-        [HttpGet("GetByCategory")]
+        [HttpGet("GetByCategory/{category}")]
         public List<Ticket> getTicketByCategory(string category)
         {
             return context.Tickets.Where(t => t.Category == category).ToList();
         }
 
+        [http]
+
+        [HttpPost("AddTicket")]
+        public Ticket AddTicket(string title, string details, string status, int submittedUserId, DateTime submittedDate,
+            int? favoritedUserId, int? resolvedUserId, string? resolutionDetails, DateTime? resolvedDate, string category)
+        {
+            Ticket newTicket = new Ticket();
+            newTicket.Title = title;
+            newTicket.Details = details;
+            newTicket.Status = status;
+            newTicket.SubmittedUserId = submittedUserId;
+            newTicket.SubmittedDate = submittedDate;
+            newTicket.FavoritedUserId = favoritedUserId;
+            newTicket.ResolvedUserId = resolvedUserId;
+            newTicket.ResolutionDetails = resolutionDetails;
+            newTicket.ResolvedDate = resolvedDate;
+            newTicket.Category = category;
+            context.Tickets.Add(newTicket);
+            context.SaveChanges();
+
+            return newTicket;
+        }
+
+        [HttpDelete("DeleteTicket/{id}")]
+        public Ticket DeleteTicket(int id)
+        {
+            Ticket ticket = context.Tickets.FirstOrDefault(t => t.TicketId == id);
+            context.Tickets.Remove(ticket);
+            context.SaveChanges();
+
+            return ticket;
+        }
     }
 }
